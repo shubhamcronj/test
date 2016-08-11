2f2191798eba2c838d1592f3d656704fb2326276
CronJ <cronj@CronJs-MacBook-Air.local>
2016-08-11 18:49:32 +0530

    First commit at office

---
+++ js/hr.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+if(localStrorage.auth == false)
+{
+	alert("go back");
+	window.location.href = 'login.html';
+}
+};
---
+++ js/login.js
@@ -0,0 +1,19 @@
+// declaring hr userid and password
+var hr_id = "cronj";
+var hr_pass = "hr";
+localStorage.setItem("auth", false);
+// authentication function
+function authenticate()
+{
+	var id = document.getElementById("id").value;
+	var pass = document.getElementById("pass").value;
+	if(id == hr_id && pass == hr_pass)
+	{
+		window.location.href = 'hr.html';
+		localStorage.setItem("auth", true);
+	}
+	else
+	{
+		alert("FAIL "+ id + " " + pass);
+	}
+}
